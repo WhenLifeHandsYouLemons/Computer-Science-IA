55e77500781cc46156b29f0783d0e357315e1c6f
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-08-26 19:48:51 +0800

    Testing

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from flask import *
+import proccess_image
+# import werkzeug
+
+app = Flask(__name__)
+
+uploads_dir = os.path.join("", "uploads")
+os.makedirs(uploads_dir, exist_ok=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/upload_success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['uploaded_file']
+        f.save(os.path.join(uploads_dir, f.filename))
+        # Only if you have werkzeug.secure_filename() working
+        # f.save(f"//prcessing_images/{werkzeug.secure_filename(f.filename())}")
+        return render_template("upload_success.html", name=f.filename)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("error_404.html")
+
+
+app.run(host='localhost', port=8080, debug=True)
