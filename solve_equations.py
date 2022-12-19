0935eae212e11982aba805ab0684d0811bb181e1
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-12-19 16:44:55 +0800

    Added multiple methods of solving equations

--- solve_equations.py
+++ solve_equations.py
@@ -1,12 +1,15 @@
 import sympy as sp  # https://problemsolvingwithpython.com/10-Symbolic-Math/10.06-Solving-Equations/
 
 def solver(equation):
-    if "=" in equation:
-        sympy_eq = sp.sympify("Eq(" + equation.replace("=", ",") + ")") # https://stackoverflow.com/questions/50043189/solve-equation-string-with-python-to-every-symbol/50047781#50047781
-        return sp.solve(sympy_eq)
-    else:
-        return eval(equation)
+    try:
+        if "=" in equation:
+            sympy_eq = sp.sympify("Eq(" + equation.replace("=", ",") + ")") # https://stackoverflow.com/a/50047781
+            return f"$$ = {sp.solve(sympy_eq)} $$"
+        else:
+            return f"$$ = {eval(equation)} $$" # type: ignore
+    except:
+        return "The input doesn't seem to be correct, try typing out the equation"
 
 
 # Example
-print(solver("2*x+6=12"))
+# print(solver("2*x+6=12"))
