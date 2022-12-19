746dd8eaf5a642c99a54a4573403c09d45fc3c2e
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-12-19 13:44:59 +0800

    Removed dependency on imutils

--- augment_image.py
+++ augment_image.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import imutils
 
 
 # Gets the bounding boxes around each number or symbol
@@ -8,7 +7,8 @@ def get_bounding_boxes(image_path):
     # Read image from which text needs to be extracted
     image = cv2.imread(image_path)
 
-    image[image > 100] = 255  # type: ignore    # https://stackoverflow.com/questions/19666626/replace-all-elements-of-python-numpy-array-that-are-greater-than-some-value/19666680#19666680
+    # Make the edges sharper by removing the gradient between the writing and the background
+    image[image > 100] = 255    # https://stackoverflow.com/a/19666680
 
     # Convert the image to gray scale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -19,7 +19,7 @@ def get_bounding_boxes(image_path):
     # Specify structure shape and kernel size.
     # Kernel size increases or decreases the area of the rectangle to be detected.
     # A smaller value like (10, 10) will detect each word instead of a sentence.
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 30))
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 45))
 
     # Applying dilation on the threshold image
     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
@@ -27,6 +27,7 @@ def get_bounding_boxes(image_path):
     # Finding contours
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    # Return an array of contours
     return contours
 
 # Uses the bounding boxes to crop the image into seperate numbers or symbols
@@ -76,12 +77,21 @@ def get_chars(img_path, contours, debug=False):
                         value=[255, 255, 255]
                     )
 
+            # Resize image to (100, 100) if needed  # https://github.com/PyImageSearch/imutils/blob/master/imutils/convenience.py
+            inter = cv2.INTER_AREA
+            dim = None
+            (h, w) = thresh.shape[:2]
+            width, height = 100, 100
             # if the width is greater than the height, resize along the width dimension
             if tW > tH:
-                thresh = imutils.resize(thresh, width=100, height=100)
+                r = width / float(w)
+                dim = (width, int(h * r))
+                thresh = cv2.resize(thresh, dim, interpolation=inter)
             # otherwise, resize along the height
             else:
-                thresh = imutils.resize(thresh, height=100)
+                r = height / float(h)
+                dim = (int(w * r), height)
+                thresh = cv2.resize(thresh, dim, interpolation=inter)
 
             # Get resized image dimensions and then find out how much width and height needed so our image will be 28x28
             (tH, tW) = thresh.shape # type: ignore
@@ -92,41 +102,51 @@ def get_chars(img_path, contours, debug=False):
             padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY, left=dX, right=dX, borderType=cv2.BORDER_CONSTANT, value=(255, 255, 255))
             padded = cv2.resize(padded, (100, 100))
 
+            padded[padded > 75] = 255
+
             chars.append(padded)
 
             if debug is True:
                 cv2.imshow("", img)
+                # cv2.imshow("", padded)
                 cv2.imshow("", rect)
                 cv2.waitKey(0)
         i += 1
     return chars
 
-# Sort the bounding boxes from left to right in the way they appear on screen
+# Sort the bounding boxes (contours) from left to right in the way they appear on screen
 def sort_chars(contours):
-    # Get all the x-positions of the bounding boxes
+    # Get all the x positions of the bounding boxes
     all_x = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         all_x.append(x)
 
+    # Convert to a numpy array
     contours = np.array(contours, dtype=object)
 
     # Bubble sort the array
     sorted = False
     while sorted is False:
         sorted = True
+        # Go through each item in the all_x array
         for pos in range(len(all_x) - 1):
+            # Check whether the current item is larger than the next one
             if all_x[pos] > all_x[pos + 1]:
                 # Use all_x as a basis to sort the contours array
+                # Switch the items in the all_x array
                 temp_x = all_x[pos]
                 all_x[pos] = all_x[pos + 1]
                 all_x[pos + 1] = temp_x
+                # Switch the items in the contours array
                 temp_contour = contours[pos]
                 contours[pos] = contours[pos + 1]
                 contours[pos + 1] = temp_contour
 
+        # Check whether the array is sorted
         for i in range(len(all_x)-1):
             if all_x[i] > all_x[i + 1]:
                 sorted = False
 
+    # Return sorted array
     return contours
